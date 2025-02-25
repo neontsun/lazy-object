f353b964b10a39a10476f78def48caa0f7dee61d
Антон Круглов <usualtsun@gmail.com>
2025-02-25 17:44:56 +0300

    feat: Add lazy interface for create in object;

---
+++ src/Contract/LazyInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Neontsun\LazyObject\Contract;
+
+use Neontsun\LazyObject\Contract\Builder\LazyGhostBuilderInterface;
+
+interface LazyInterface
+{
+    /**
+     * @return LazyGhostBuilderInterface<static>
+     */
+    public static function lazy(LazyObjectFactoryInterface $factory): LazyGhostBuilderInterface;
+
+    /**
+     * Checks if the model is a lazy loading model and if the fields that are lazy are not initialized.
+	 * Returns true if at least one field marked as lazy has been loaded.
+     */
+    public function isUninitialized(): bool;
+}
